0c9ca7f46d7b2f1333df8d58d3440bb42ef3a2bd
stnjumu <1282807292@qq.com>
2020-12-27 21:49:02 +0800

    Web connected and send message test

--- src/main/java/nju/hulugame/client/GameClient.java
+++ src/main/java/nju/hulugame/client/GameClient.java
@@ -1,13 +1,100 @@
 package nju.hulugame.client;
 
-public class GameClient {
+import java.net.*;
+import java.io.*;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import nju.hulugame.server.GameServer;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class GameClient{
+
+    private int UDP_PORT=40001;//客户端的UDP端口号
+    private String serverIP;//服务器IP地址
+    private int serverUDPPort;//服务器转发客户但UDP包的UDP端口
+    private int TANK_DEAD_UDP_PORT;//服务器监听坦克死亡的UDP端口
+    private DatagramSocket dSocket = null;//客户端的UDP套接字
     public static void main(String[] args) {
         GameClient gc=new GameClient();
-        gc.start();
+        gc.connect("127.0.0.1");
     }
 
-    public void start() {
-        System.out.println("Game Client Started!");
+    public void connect(String ip){
+        serverIP = ip;
+        Socket s = null;
+        try {
+            dSocket = new DatagramSocket(UDP_PORT);//创建UDP套接字
+            try {
+                s = new Socket(ip, GameServer.TCP_PORT);//创建TCP套接字
+            }
+            catch (Exception e1){
+                e1.printStackTrace();
+            }
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            dos.writeInt(UDP_PORT);//向服务器发送自己的UDP端口号
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            int id = dis.readInt();//获得自己的id号
+            System.out.println("获得id= "+id);
+            serverUDPPort= dis.readInt();  // 获得服务器端口号;
+            System.out.println("获得服务器port = "+serverUDPPort);
+            System.out.println("connect to server successfully...");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try{
+                if(s != null) s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        // TankNewMsg msg = new TankNewMsg(tc.getMyTank());//创建坦克出生的消息
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(88);
+        DataOutputStream dos = new DataOutputStream(baos);
+        try {
+            dos.writeInt(120);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        byte[] buf = baos.toByteArray();
+        try{
+            DatagramPacket dp = new DatagramPacket(buf, buf.length, new InetSocketAddress(serverIP, serverUDPPort));
+            dSocket.send(dp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(new UDPThread()).start();//开启客户端UDP线程, 向服务器发送或接收游戏数据
+    }
+
+    private class UDPThread implements Runnable{
+
+        byte[] buf = new byte[1024];
+
+        @Override
+        public void run() {
+
+            while (null != dSocket){
+                DatagramPacket dp = new DatagramPacket(buf, buf.length);
+                try {
+                    dSocket.receive(dp);
+                    byte[] ibuf= dp.getData();
+                    ByteArrayInputStream bais = new ByteArrayInputStream(ibuf, 0, dp.getLength());
+                    DataInputStream dis = new DataInputStream(bais);
+                    int msg=dis.readInt();
+                    System.out.println("收到消息："+msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- src/main/java/nju/hulugame/server/GameServer.java
+++ src/main/java/nju/hulugame/server/GameServer.java
@@ -1,13 +1,120 @@
 package nju.hulugame.server;
 
-public class GameServer {
+import java.util.ArrayList;
 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+import java.net.*;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class GameServer{
+
+    public static int ID = 100;  // id号的初始序列
+    public static final int TCP_PORT = 50000;  // TCP端口号
+    public static final int UDP_PORT = 50001;  // 转发客户端数据的UDP端口号
+    public static final int TANK_DEAD_UDP_PORT = 50002;//接收客户端坦克死亡的端口号
+    
+    private ArrayList<Client> clients = new ArrayList<>(); //客户端集合；
     public static void main(String[] args) {
-        GameServer gs=new GameServer();
+        
+        GameServer gs= new GameServer();
         gs.start();
     }
 
-    public void start() {
-        System.out.println("Game Server Started!");
+    public void start(){
+        new Thread(new UDPThread()).start();
+        ServerSocket ss = null;
+        try {
+            ss = new ServerSocket(TCP_PORT);//在TCP欢迎套接字上监听客户端连接
+            System.out.println("TankServer has started...");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while(true){
+            Socket s = null;
+            try {
+                s = ss.accept();//给客户但分配专属TCP套接字
+                System.out.println("A client has connected...");
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                int cPort = dis.readInt();//记录客户端UDP端口
+                System.out.println("UDPport: "+cPort);
+                Client client = new Client(s.getInetAddress().getHostAddress(), cPort, ID);//创建Client对象
+                clients.add(client);//添加进客户端容器
+
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+                dos.writeInt(ID++);//向客户端分配id号
+                dos.writeInt(UDP_PORT);//告诉客户端自己的UDP端口号
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    if(s != null) s.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private class UDPThread implements Runnable{
+
+        byte[] buf = new byte[1024];
+
+        @Override
+        public void run() {
+            DatagramSocket dSocket = null;
+            try{
+                System.out.println("监听UDP port: "+UDP_PORT);
+                dSocket = new DatagramSocket(UDP_PORT);
+            }catch (SocketException e) {
+                e.printStackTrace();
+            }
+
+            while (null != dSocket){
+                DatagramPacket dp = new DatagramPacket(buf, buf.length);
+                try {
+                    dSocket.receive(dp);
+                    byte[] ibuf= dp.getData();
+                    ByteArrayInputStream bais = new ByteArrayInputStream(ibuf, 0, dp.getLength());
+                    DataInputStream dis = new DataInputStream(bais);
+                    int msg=dis.readInt();
+                    System.out.println("收到消息："+msg);
+                    for (Client c : clients){   // 收到什么就发什么；
+                        dp.setSocketAddress(new InetSocketAddress(c.IP, c.UDP_PORT));
+                        dSocket.send(dp);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public class Client{
+        String IP;
+        int UDP_PORT;
+        int id;
+
+        public Client(String ipAddr, int UDP_PORT, int id) {
+            this.IP = ipAddr;
+            this.UDP_PORT = UDP_PORT;
+            this.id = id;
+        }
     }
 }
